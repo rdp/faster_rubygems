2ca1ae5eff8382fa90aaf132ca8d7e3d8c1de154
rogerdpack <rogerpack2005@gmail.com>
2009-05-09 14:06:07 -0600

    make install work in 1.9, too

--- install.rb
+++ install.rb
@@ -2,14 +2,20 @@ begin
  require 'ftools'
 rescue LoadError
  # not there on 1.9
+ require 'fileutils'
+ File.class_eval {
+  def File.copy *args
+    File.copy_stream *args
+  end
+ }
 end
 
 require 'rbconfig'
 dir = File.dirname(__FILE__)
 for file in ['rubygems_fast.rb', 'rubygems_f.rb'] do
-  File.copy dir + '/' + file, Config::CONFIG['sitelibdir']
+  File.copy dir + '/' + file, Config::CONFIG['sitelibdir'] + '/' + file
 end
 
-require 'rubygems_f' # test it out :P
+require 'rubygems_f' # test it out now :P
 
 puts 'Installed--thank you for trying out require \'rubygems_f\''
