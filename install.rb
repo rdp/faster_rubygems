cd75b7c1a35d3666fd6edf024da3eab561bd82bb
rogerdpack <rogerpack2005@gmail.com>
2009-09-17 16:36:08 -0600

    ahh better rubygems fast

--- install.rb
+++ install.rb
@@ -1,21 +1,11 @@
-begin
- require 'ftools'
-rescue LoadError
- # not there on 1.9
- require 'fileutils'
- File.class_eval {
-  def File.copy *args
-    File.copy_stream(*args)
-  end
- }
-end
-
+require 'fileutils'
 require 'rbconfig'
+
 dir = File.dirname(__FILE__)
-for file in ['rubygems_fast.rb', 'rubygems_f.rb'] do
-  File.copy dir + '/' + file, Config::CONFIG['sitelibdir'] + '/' + file
+for file in ['rubygems_fast.rb', 'frubygems.rb'] do
+  FileUtils.cp dir + '/' + file, Config::CONFIG['sitelibdir'] + '/' + file
 end
 
-require 'rubygems_f' # test it out now :P
+require 'frubygems' # test it out now :P
 
-puts 'Installed--thank you for trying out require \'rubygems_f\''
+puts 'Installed--thank you for trying out -- require \'frubygems\''
