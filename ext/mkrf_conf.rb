76cd34c3626c9d098c882ff5873a9b65c023380f
rdp <rogerpack2005@gmail.com>
2009-10-29 16:18:21 -0600

    update

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -1,20 +1,5 @@
- require 'rubygems'
-  require 'rubygems/command.rb'
-  require 'rubygems/dependency_installer.rb' 
-  begin
-    Gem::Command.build_args = ARGV
-    rescue NoMethodError
-  end 
-  inst = Gem::DependencyInstaller.new
-  begin
-    if RUBY_VERSION < "1.9"
-      inst.install "ruby-debug-base", "~> 0.10.3"
-    else
-      inst.install "ruby-debug-base19", "~> 0.11.24"
-    end
-    rescue
-      exit(1)
-  end 
+puts 'in mkrf'
+load File.dirname(__FILE__) + "/install.rb" # install it for them
 
   f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")   # create dummy rakefile to indicate success
   f.write("task :default\n")
