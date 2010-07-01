c7583dcb803d029ab750ad007684bf6e7e9eb76f
rogerdpack <rogerpack2005@gmail.com>
2010-07-01 15:50:03 -0600

    more descriptive end user messages

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,6 +1,7 @@
 Gem.post_install {
   # accomodate for when they install more than one at a time...
   # a straight require won't cut it...
+  print 'because of new gem installation '
   success = require "faster_rubygems/create_cache_for_all"
   if !success
     Gem.create_cache_for_all!  
@@ -8,6 +9,7 @@ Gem.post_install {
 }
 
 Gem.post_uninstall {
+  print 'because of gem uninstallation '
   success = require "faster_rubygems/create_cache_for_all"
   if !success
     Gem.create_cache_for_all!  
@@ -15,7 +17,7 @@ Gem.post_uninstall {
 }
 
 Gem.pre_uninstall { |gem_installer_instance|
-  
+  puts 'removing faster_rubygems as the default for require rubygems...'
   if gem_installer_instance.spec.name == 'faster_rubygems' && RUBY_VERSION[0..2] == '1.8'
     begin
        require "faster_rubygems/unoverride" # just in case
